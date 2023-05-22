9b79480935d5229480fd7ea4756d5b12ad8241c0
cartwatson <cartwatson@gmail.com>
2023-05-22 15:38:02 -0600

    added first three algs

--- 5400-computer-graphics/2assign-curves/scripts/driver.js
+++ 5400-computer-graphics/2assign-curves/scripts/driver.js
@@ -10,6 +10,7 @@ MySample.main = (function(graphics) {
     //
     //------------------------------------------------------------------
     function update(elapsedTime) {
+        
     }
 
     //------------------------------------------------------------------
@@ -38,6 +39,14 @@ MySample.main = (function(graphics) {
     }
 
     console.log('initializing...');
-    requestAnimationFrame(animationLoop);
+    // requestAnimationFrame(animationLoop);
+    // DEBUG
+    graphics.drawCurve(0, [{x: 100, y: 500}, {x: 900, y: 500}, {x: 200, y: 0}, {x: -200, y: 0}], 100, true, true, false, "aqua")
+    graphics.drawCurve(0, [{x: 100, y: 500}, {x: 900, y: 500}, {x: 200, y: 500}, {x: -200, y: -500}], 100, true, false, false, "lightpink")
+    graphics.drawCurve(1, [
+        {x:   0, y: 500}, {x: 100, y: 300}, {x: 200, y: 700}, {x: 300, y: 200}, {x: 400, y: 800},
+        {x: 500, y: 400}, {x: 600, y: 600}, {x: 700, y: 500}, {x: 800, y: 500}, {x: 900, y: 700}
+    ], 10, true, false, false, "white")
+    // END DEBUG
 
 }(MySample.graphics));
--- 5400-computer-graphics/2assign-curves/scripts/graphics.js
+++ 5400-computer-graphics/2assign-curves/scripts/graphics.js
@@ -174,11 +174,66 @@ MySample.graphics = (function(pixelsX, pixelsY, showPixels) {
 
     //------------------------------------------------------------------
     //
+    // Handles boolean logic for curve drawing
+    //
+    //------------------------------------------------------------------
+    function drawPointsLineControl(startPt, endPt, points, showPoints, showLine, showControl, lineColor) {
+        // print line
+        if (showLine) {
+            for (let i = 0; i < points.length - 1; i++) {
+                // grab point and next point
+                let start = { x: points[i].x, y: points[i].y }
+                let end   = { x: points[i+1].x, y: points[i+1].y }
+                
+                console.log(start.x, start.y, end.x, end.y) // DEBUG
+                // drawLine(start.x, start.y, end.x, end.y, lineColor) // TODO: FIX
+            }
+        }
+
+        // print points
+        if (showPoints) {
+            for (let i = 0; i < points.length; i++) { drawPoint(points[i].x, points[i].y, lineColor) }
+        // print control points
+        } else if (showControl) {
+            drawPoint(startPt.x, startPt.y, lineColor)
+            drawPoint(endPt.x, endPt.y, lineColor)
+        }
+    }
+
+    
+    //------------------------------------------------------------------
+    //
     // Renders an Hermite curve based on the input parameters.
     //
     //------------------------------------------------------------------
     function drawCurveHermite(controls, segments, showPoints, showLine, showControl, lineColor) {
-        
+        // parse controls & init
+        let startPt    = { x: controls[0].x, y: controls[0].y }
+        let tanStartPt = { x: controls[1].x, y: controls[1].y }
+        let tanEndPt   = { x: controls[2].x, y: controls[2].y }
+        let endPt      = { x: controls[3].x, y: controls[3].y }
+        let points = []
+
+        // divide curve into segments
+        for (let t = 0; t <= 1; t += 1 / segments) {
+            // calc t^2 and t^3
+            let t2 = t  * t
+            let t3 = t2 * t
+
+            // calculate basis functions
+            let h1 =  2 * t3 - 3 * t2 + 1
+            let h2 = -2 * t3 + 3 * t2
+            let h3 =      t3 - 2 * t2 + t
+            let h4 =      t3 -     t2
+
+            // calc x and y & store point
+            points.push({
+                x: h1 * startPt.x + h2 * endPt.x + h3 * tanStartPt.x + h4 * tanEndPt.x,
+                y: h1 * startPt.y + h2 * endPt.y + h3 * tanStartPt.y + h4 * tanEndPt.y,
+            })
+        }
+
+        drawPointsLineControl(startPt, endPt, points, showPoints, showLine, showControl, lineColor)
     }
 
     //------------------------------------------------------------------
@@ -187,7 +242,40 @@ MySample.graphics = (function(pixelsX, pixelsY, showPixels) {
     //
     //------------------------------------------------------------------
     function drawCurveCardinal(controls, segments, showPoints, showLine, showControl, lineColor) {
+        let points = []
+        let startPt = controls[0]
+        let endPt   = controls.slice(-1)
+        let tension = 0.5 //TODO: TEST
 
+        // calculate points
+        for (let i = 0; i < controls.length - 4; i++) {
+            let p0 = controls[i+0]
+            let p1 = controls[i+1]
+            let p2 = controls[i+2]
+            let p3 = controls[i+3]
+
+            // divide curve into segments
+            for (let t = 0; t <= 1; t += 1 / segments) {
+                // calc t^2 and t^3
+                let t2 = t  * t
+                let t3 = t2 * t
+
+                // calculate basis functions
+                let h1 =  2 * t3 - 3 * t2 + 1
+                let h2 = -2 * t3 + 3 * t2
+                let h3 =      t3 - 2 * t2 + t
+                let h4 =      t3 -     t2
+
+                // calc x and y & store point
+                points.push({
+                    x: h1 * p1.x + h2 * p2.x + h3 * (p2.x - p0.x) * tension + h4 * (p3.x - p1.x) * tension,
+                    y: h1 * p1.y + h2 * p2.y + h3 * (p2.y - p0.y) * tension + h4 * (p3.y - p1.y) * tension,
+                })
+            }
+        }
+
+        // boolean logic here
+        drawPointsLineControl(startPt, endPt, points, showPoints, showLine, showControl, lineColor)
     }
 
     //------------------------------------------------------------------
@@ -196,7 +284,32 @@ MySample.graphics = (function(pixelsX, pixelsY, showPixels) {
     //
     //------------------------------------------------------------------
     function drawCurveBezier(controls, segments, showPoints, showLine, showControl, lineColor) {
+        let points  = []
+        let startPt = controls[0]
+        let endPt   = controls.slice(-1)
 
+        let len = controls.length - 1
+        for (let t = 0; t <= 1; t += 1 / segments) {
+            for (let i = 0; i < len; i++) {
+                coefficient = binomialCoefficient(len, i) * (1 - t)**(len - i) * t**i
+                points.push({
+                    x: coefficient * controlPoints[i].x,
+                    y: coefficient * controlPoints[i].y
+                })
+            }
+        }
+
+        // boolean logic here
+        drawPointsLineControl(startPt, endPt, points, showPoints, showLine, showControl, lineColor)
+    }
+
+    // Helper function to calculate the binomial coefficient of two numbers
+    // adjusted from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-20.php
+    function binomialCoefficient(n, k) {
+        let coeff = 1;
+        for (let x = n-k+1; x <= n; x++) { coeff *= x }
+        for (x = 1; x <= k; x++) { coeff /= x }
+        return coeff;
     }
 
     //------------------------------------------------------------------
