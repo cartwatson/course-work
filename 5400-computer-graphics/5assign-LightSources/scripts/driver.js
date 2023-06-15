9b2b0c9849b9a4a460c06517a6eba237082d5e19
cartwatson <cartwatson@gmail.com>
2023-06-14 21:31:04 -0600

    finished!

--- 5400-computer-graphics/5assign-LightSources/scripts/driver.js
+++ 5400-computer-graphics/5assign-LightSources/scripts/driver.js
@@ -13,9 +13,6 @@ MySample.main = (function() {
     const vertexShaderFilePath = '../shaders/simple.vert'
     const fragmentShaderFilePath = '../shaders/simple.frag'
 
-    // testings
-    let colors = new Float32Array([1.0, 0.0, 0.0]);
-
     // shaders
     let vertexShader = null;
     let fragmentShader = null;
@@ -35,21 +32,12 @@ MySample.main = (function() {
         // Step 3 : Prepare Raw Data
         bunny = parsePly(data);
 
-        // Step 4 : Prepare Buffer Objects
-        // vertex buffer
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, bunny.vertices, gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        // index buffer
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, bunny.indices, gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
-        // normals buffer
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, bunny.normals, gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        return loadFileFromServer(dragonFilePath)
+    }).then(data => {
+        // Step 3 : Prepare Raw Data
+        dragon = parsePly(data);
 
-        // Step 5 : Prepare Shaders
+        // Step 5 : Prepare Shaders & Shader Program
         // vertex shader
         return loadFileFromServer(vertexShaderFilePath);
     }).then(source => {
@@ -74,6 +62,7 @@ MySample.main = (function() {
         
         // Step 7 : Request Animation Frame
     }).then(() => {
+        changeModel(bunny);
         requestAnimationFrame(animationLoop);
     });
 
@@ -81,22 +70,55 @@ MySample.main = (function() {
     //
     // Scene updates go here.
     // Variables used:
+    // iteration control
     let theta = 0;
-    let t = 0; // to function as a time variable, changes model and lighting
-    let model = bunny;
+    let time = 0; // to function as a time variable, changes model and lighting
+    // uTRS
+    let scale = 4;
+    let transform = {dx: 0, dy: -0.3, dz: 0}
+    // model currently being rendered
+    let model = null; // holds info for current model displayed
+    // lights
+    let light1 = [1, 0, 0]; // default values: [1, 0, 0];
+    let light2 = [0, 0, 0]; // default values: [0, 1, 0];
+    let light3 = [0, 0, 0]; // default values: [0, 0, 1];
     //
     //------------------------------------------------------------------
     function update() {
-        // change model
+        // ---update model and lights based on time---------------------
+        if (time < 25) {
+            // all of these values are set with an init or reset
+        } else if (time < 50) {
+            // just the second light
+            light1 = [0, 0, 0];
+            light2 = [0, 1, 0];
+            light3 = [0, 0, 0];
+        } else if (time < 75) {
+            // just the third light
+            light1 = [0, 0, 0];
+            light2 = [0, 0, 0];
+            light3 = [0, 0, 1];
+        } else if (time < 100) {
+            // all lights should be on
+            light1 = [1, 0, 0];
+            light2 = [0, 1, 0];
+            light3 = [0, 0, 1];
+        } else { // reset
+            time = 0;
+            // switch model
+            if (model.vertices.length == 107841) { changeModel(dragon); }
+            else { changeModel(bunny) }
+            // just the first light
+            light1 = [1, 0, 0];
+            light2 = [0, 0, 0];
+            light3 = [0, 0, 0];
+        }
 
-        // change lights
-
-        t += 1;
-        if (t > 500) { t = 0; }
-
-
+        // increment loop values
+        time += 0.05;
         theta += 0.01;
 
+        // ---matrices for Translation, Scale, and Rotation-------------
         let uR_Y = [
             Math.cos(theta), 0, Math.sin(theta), 0,
             0, 1, 0, 0,
@@ -104,25 +126,65 @@ MySample.main = (function() {
             0, 0, 0, 1,
         ];
 
-        // let uR_X = [
-        //     1, 0, 0, 0,
-        //     0, Math.cos(theta), -Math.sin(theta), 0,
-        //     0, Math.sin(theta), Math.cos(theta), 0,
-        //     0, 0, 0, 1,
-        // ];
+        let uR_X = [
+            1, 0, 0, 0,
+            0, Math.cos(theta), -Math.sin(theta), 0,
+            0, Math.sin(theta), Math.cos(theta), 0,
+            0, 0, 0, 1,
+        ];
 
-        // let uR_Z = [
-        //     Math.cos(theta), -Math.sin(theta), 0, 0,
-        //     Math.sin(theta), Math.cos(theta), 0, 0,
-        //     0, 0, 1, 0,
-        //     0, 0, 0, 1,
-        // ];
+        let uR_Z = [
+            Math.cos(theta), -Math.sin(theta), 0, 0,
+            Math.sin(theta), Math.cos(theta), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        ];
 
-        let uRotation = uR_Y;//multiplyMatrix4x4(multiplyMatrix4x4(uR_Y, uR_X), uR_Z);
-        // let uRotation = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]; // IDENTITY MATRIX, just for testing
+        let uTS = [
+            scale, 0, 0, 0,
+            0, scale, 0, 0,
+            0, 0, scale, 0,
+            transform.dx, transform.dy, transform.dz, 1,
+        ]
 
+        // calculate uTSR (uTranslateScaleRotation)
+        // let uRotation = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]; // TESTING: i matrix
+        let uRotation = uR_Y;//multiplyMatrix4x4(multiplyMatrix4x4(uR_Y, uR_X), uR_Z);//uR_Y;
+        let uTSR = multiplyMatrix4x4(uRotation, uTS);
+
+        //variables for projection
+        let n = 0;
+        let t = 0;
+        let f = 0;
+
+        // matrix for perspective projection
+        let projection = [
+            // n/r, 0, 0, 0,
+            // 0, n/t, 0, 0,
+            // 0, 0, (-(f + n))/(f - n), (-2 * f * n)/(f - n),
+            // 0, 0, -1, 0,
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        ];
+
+        // ---update values in shader-----------------------------------
+        // update lights
+        let shaderLight1 = gl.getUniformLocation(shaderProgram, 'L1_d');
+        let shaderLight2 = gl.getUniformLocation(shaderProgram, 'L2_d');
+        let shaderLight3 = gl.getUniformLocation(shaderProgram, 'L3_d');
+        gl.uniform3fv(shaderLight1, light1);
+        gl.uniform3fv(shaderLight2, light2);
+        gl.uniform3fv(shaderLight3, light3);
+
+        // update location
         let location = gl.getUniformLocation(shaderProgram, 'uThing');
-        gl.uniformMatrix4fv(location, false, uRotation);
+        gl.uniformMatrix4fv(location, false, uTSR);
+
+        // update with projection perspective
+        let perspective = gl.getUniformLocation(shaderProgram, 'uProjection');
+        gl.uniformMatrix4fv(perspective, false, projection);
     }
 
     //------------------------------------------------------------------
@@ -135,12 +197,12 @@ MySample.main = (function() {
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         let position = gl.getAttribLocation(shaderProgram, 'aPosition');
         gl.enableVertexAttribArray(position);
-        gl.vertexAttribPointer(position, 3, gl.FLOAT, false, bunny.vertices.BYTES_PER_ELEMENT * 3, 0);
+        gl.vertexAttribPointer(position, 3, gl.FLOAT, false, model.vertices.BYTES_PER_ELEMENT * 3, 0);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
         let normal = gl.getAttribLocation(shaderProgram, 'aNormal');
         gl.enableVertexAttribArray(normal);
-        gl.vertexAttribPointer(normal, 3, gl.FLOAT, false, bunny.normals.BYTES_PER_ELEMENT * 3, 0);
+        gl.vertexAttribPointer(normal, 3, gl.FLOAT, false, model.normals.BYTES_PER_ELEMENT * 3, 0);
 
         // Step 8 : Reset Framebuffer & Depth Buffer
         gl.clearColor(0.392156862740980392156862745098, 0.58431372549019607843137254901961, 0.92941176470588235294117647058824, 1.0);
@@ -151,7 +213,7 @@ MySample.main = (function() {
 
         // Step 9 : Render the models
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        gl.drawElements(gl.TRIANGLES, bunny.indices.length, gl.UNSIGNED_INT, 0);
+        gl.drawElements(gl.TRIANGLES, model.indices.length, gl.UNSIGNED_INT, 0);
     }
 
     //------------------------------------------------------------------
@@ -167,4 +229,26 @@ MySample.main = (function() {
         requestAnimationFrame(animationLoop);
     }
 
+    //------------------------------------------------------------------
+    //
+    // Helper function to set model and ...
+    //     ... redo vertex, index, and normal buffer
+    //
+    //------------------------------------------------------------------
+    function changeModel(newModel) {
+        model = newModel;
+        // Step 4 : Prepare Buffer Objects
+        // vertex buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, model.vertices, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        // index buffer
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, model.indices, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+        // normals buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, model.normals, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    }
 }());
--- 5400-computer-graphics/5assign-LightSources/shaders/simple.vert
+++ 5400-computer-graphics/5assign-LightSources/shaders/simple.vert
@@ -1,37 +1,36 @@
 #version 300 es
 
-//uniform mat4 uProjection;
+uniform mat4 uProjection;
 uniform mat4 uThing;
 in vec4 aPosition;
 in vec4 aNormal;
 out vec4 vColor;
 
 // light intensity = light color RGB
-// TODO: take this in as a uniform later
-vec3 L1_d = vec3(1, 0, 0);
-vec3 L2_d = vec3(0, 1, 0);
-vec3 L3_d = vec3(0, 0, 1);
+uniform vec3 L1_d;
+uniform vec3 L2_d;
+uniform vec3 L3_d;
 
 // constants
-vec3 K_d = vec3(1, 0, 0);
+vec3 K_d = vec3(0.5, 0.5, 0.5);
 vec3 LightPos1 = vec3(1, 1, 1);
-vec3 LightPos2 = vec3(-1, -1, -1);
-vec3 LightPos3 = vec3(1, -1, -1);
+vec3 LightPos2 = vec3(1, 1, 1);
+vec3 LightPos3 = vec3(1, 1, 1);
 
 // ambient light
-vec3 K_a = vec3(0.25, 0, 0);
-// vec3 La_d = vec3(1, 1, 1);
+vec3 K_a = vec3(1, 1, 1);
 vec3 La_d = vec3(0.25, 0.25, 0.25);
 
 void main()
 {
-    //gl_Position = uProjection * uThing * aPosition;
-    gl_Position = uThing * aPosition;
-    vec3 Light1 = K_d * L1_d * dot(aPosition.xyz, LightPos1 - aPosition.xyz);
-    vec3 Light2 = K_d * L2_d * dot(aPosition.xyz, LightPos2 - aPosition.xyz);
-    vec3 Light3 = K_d * L3_d * dot(aPosition.xyz, LightPos3 - aPosition.xyz);
+    // position calcs
+    gl_Position = uProjection * uThing * aPosition;
+
+    // lighting calcs
+    vec3 Light1 = K_d * L1_d * dot(aNormal.xyz, LightPos1 - aPosition.xyz);
+    vec3 Light2 = K_d * L2_d * dot(aNormal.xyz, LightPos2 - aPosition.xyz);
+    vec3 Light3 = K_d * L3_d * dot(aNormal.xyz, LightPos3 - aPosition.xyz);
     vec3 ambient = K_a * La_d;
     vec3 intensity = clamp(Light1 + Light2 + Light3 + ambient, vec3(0, 0, 0), vec3(1, 1, 1));
     vColor = vec4(intensity, 1);
-    // vColor = vec4(1, 0, 0, 1); // TESTING ONLY
 }
