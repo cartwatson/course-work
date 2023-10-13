f2b0eebbb4f7d67560bd1b37ade3decd3546fd1f
cartwatson <cartwatson@gmail.com>
2023-10-13 11:43:15 -0600

    add count_sort

--- 5030-high-performance-computing/3assign/count_sort.cpp
+++ 5030-high-performance-computing/3assign/count_sort.cpp
@@ -3,23 +3,81 @@
  * @author Carter Watson
  * @date oct-13-2023
  * @brief 
- * @todo [ ] 
+ * @todo
+ * - [X] Generate random integers in the range 1 to n (initialize with srand(100) ) 
+ * - [X] print the original sequence
+ * - [ ] sort the sequence using multithreading
+ * - [X] print the ordered sequence
  */
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <omp.h>
+#include <cstring>
+#include <cstdlib>
+
+/**
+ * @brief sorts an array of integers using the count sort algorithm and uses multithreading
+ * 
+ * @param a (int[]) the array to sort
+ * @param n (int) the number of elements in the array
+ * 
+ * @return void
+*/
+void count_sort(int a[], int n) {
+    int *temp = (int *)malloc(n * sizeof(int));
+
+    #pragma omp parallel
+    {
+        int i, j, count;
+
+        #pragma omp for schedule(dynamic)
+        for (i = 0; i < n; i++) {
+            count = 0;
+            for (j = 0; j < n; j++) {
+                if (a[j] < a[i]) {
+                    count++;
+                } else if (a[j] == a[i] && j < i) {
+                    count++;
+                }
+            }
+            temp[count] = a[i];
+        }
+    }
+
+    memcpy(a, temp, n * sizeof(int));
+    free(temp);
+}
+
+
+/**
+ * @brief print elements of vector to std::cout
+ * 
+ * @param elements (vector<int>) the vector to print
+ * @param original (bool) whether or not the vector is the original data
+ * 
+ * @return void
+ */
+void printData(std::vector<int> elements, bool original = false) {
+    std::cout << (original ? "original:" : "sorted:");
+
+    for (int i = 0; i < elements.size(); i++) {
+        std::cout << " " << elements[i];
+    }
+
+    std::cout << std::endl;
+    return;
+}
+
 
 /**
 * @brief 
 * 
 * @param number_of_threads (int) the number of threads to use for the execution
-* @param bin_count (int) the number of bins in the histogram
-* @param min_meas (float) minimum value of the measurements
-* @param max_meas (float) maximum value of the measurements
-* @param data_count (int) number of measurements
-* @note param ordering: <number of threads> <bin count> <min meas> <max meas> <data count>
+* @param number_of_elements (int) the number of elements
+* @note param ordering: <number of threads> <number of elements>
 * 
 * @return error_code (int) 
 * @retval 0 Success
@@ -35,4 +93,20 @@ int main(int argc, char *argv[]) {
     // init params
     const int NUM_THREADS = std::stoi(argv[1]);
     const int NUM_ELEMENTS =  std::stoi(argv[2]);
+
+    // init data with srand
+    srand(100);
+    std::vector<int> data;
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
+        data.push_back(std::rand() % 100);
+    }
+
+    printData(data, true); // print original data
+
+    // sort data using multithreading
+    count_sort(&data[0], NUM_ELEMENTS);
+
+    printData(data); // print sorted data
+
+    return 0;
 }
